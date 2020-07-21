f70c101616b65df065a677c450115c848e9c1851
acryliper <68606378+acryliper@users.noreply.github.com>
2020-07-22 00:07:52 +0700

    Add files via upload

---
+++ hw01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"1st Term of 2020\t\\064\065\061\063\062\061\061\064\tWanthanee Prachuabsupakit\n\n";
+	cout<<"-------------------------------------------------------------------\n";
+	cout<<"|   Subject ID  | Subject name\t\t   | Score (100) |Grade|\n";
+	cout<<"-------------------------------------------------------------------\n";
+	cout<<"|    \064\065\067\060\060\063\064    | Introduction to computer |\t  \070\065\t | \"A\" | \n";
+	cout<<"|    \064\065\070\060\062\064\067    | English\t\t   |\t  \067\064\t | \"B\" |\n";
+	cout<<"|    \064\065\063\060\060\060\061    | Design Thinking\t   |\t  \071\062\t | \"A\" |\n";
+	cout<<"-------------------------------------------------------------------\n";
+	cout<<"|     TOTAL\t| Subject \= \063\t\t   | Average \= \063.\070\067 |\n";
+	cout<<"===================================*===\n\n\nKMUTNB\n";
+
+}//main
